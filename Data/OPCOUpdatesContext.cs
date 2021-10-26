907f988d1d750c0a78f9b768d1e3be0074cf99b7
OluwaseniO <oluwaseni@sinetservices.com>
2021-10-26 22:56:46 +0100

    Add project files.

---
+++ Data/OPCOUpdatesContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OPCOUpdates.Models;
+
+namespace OPCOUpdates.Data
+{
+    public class OPCOUpdatesContext : DbContext
+    {
+        public OPCOUpdatesContext (DbContextOptions<OPCOUpdatesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OPCOUpdates.Models.Department> Department { get; set; }
+
+        public DbSet<OPCOUpdates.Models.Project> Project { get; set; }
+
+        public DbSet<OPCOUpdates.Models.Theme> Theme { get; set; }
+    }
+}
---
+++ Models/Theme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OPCOUpdates.Models
+{
+    public class Theme
+    {
+        public int Id { get; set; }
+        public string ThemeName { get; set; }
+
+    }
+}
